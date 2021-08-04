ef35e5d6f7002b7c777fde8754dc2b66ed0aa892
Katja Danilova <katja.danilova@metatavu.fi>
2021-08-04 11:59:02 +0300

    removed migrations from 21 to 14 version

--- db/migrate/20210715091972_add_enable_machine_translation_to_decidim_organizations.decidim.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-# This migration comes from decidim (originally 20200525184143)
-
-class AddEnableMachineTranslationToDecidimOrganizations < ActiveRecord::Migration[5.2]
-  def change
-    add_column :decidim_organizations, :enable_machine_translations, :boolean, default: false
-  end
-end
--- db/migrate/20210715092041_rename_budget_to_budget_ammount.decidim_budgets.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-# This migration comes from decidim_budgets (originally 20200629072626)
-
-class RenameBudgetToBudgetAmmount < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :decidim_budgets_projects, :budget, :budget_amount
-  end
-end
